824e1d7ff0fa085e58ba256e9f68e75176fdd26f
Carden Z <ephemere@protonmail.com>
2019-04-12 17:15:18 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -1,5 +1,24 @@
-# require libraries/modules here
+require 'nokogiri'
+require 'pry'
 
 def create_project_hash
-  # write your code here
+  html = File.read('fixtures/kickstarter.html')
+  kickstarter = Nokogiri::HTML(html)
+
+  projects = {}
+
+  kickstarter.css('li.project.grid_4').each do |project|
+    title = project.css("h2.bbcard_name strong a").text
+    projects[title.to_sym] = {
+      image_link: project.css("div.project-thumbnail img").attribute('src').value,
+      description: project.css("p.bbcard_blurb").text,
+      loaction: project.css("span.location-name").text,
+      percent_funded: project.css("li.funded strong").text.gsub('%', '')
+    }
+  end
+  projects
+
+
+
 end
+    # creator = project.css("h2.bbcard_name span").text.gsub('\n', '')
